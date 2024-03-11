9c6086b4da996289f3123cd7b51094295c83b53f
Damayanti Nama <namabrijrajnama@gmail.com>
2024-03-11 19:50:55 +0530

    Add files via upload

--- app/config/webpack.dev.js
+++ app/config/webpack.dev.js
@@ -1,1 +1,18 @@
+const path = require("path");
+const webpack = require("webpack");
 
+module.exports = {
+  entry: { "app.utils": ["./src_style/js/main"] },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "../..", "public/js/"),
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".js"],
+    modules: ["node_modules", path.resolve(__dirname, "..", "src_style")],
+  },
+  resolveLoader: {
+    modules: ["node_modules"],
+  },
+};
